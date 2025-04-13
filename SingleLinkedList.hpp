5b274d820a31311a2e103721d8b76dbd44c2a58d
280051hubert <280051@student.pwr.edu.pl>
2025-04-13 09:46:59 +0200

    SingleLinkedList.hpp updated. compiles

--- SingleLinkedList.hpp
+++ SingleLinkedList.hpp
@@ -1,12 +1,10 @@
 #pragma once
-
 #include "List.hpp"
 
 template <typename T>
 class SingleLinkedList : public List<T> {
 private:
-	struct Node
-	{
+	struct Node {
 		T dane;
 		Node* next;
 
@@ -15,16 +13,17 @@ private:
 
 	Node* head;
 	Node* tail;
-	size_t size_t;
+	size_t _size;
+	void clear();
 
 public:
 	SingleLinkedList();
-
 	~SingleLinkedList();
 
 	void push_end(const T& value);
-
 	void push_front(const T& value);
+	void push_random(const T& value);
+	void find_random(const T& value);
 
 	bool pop_front();
 };
